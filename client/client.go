fc3c9bff716cb048d6b61130a87d738d135c06e7
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2026-02-16 06:51:36 -0600

    Update client/client.go
    Apply suggestion to use GetExitCode() method.
    
    Co-authored-by: Marek Smoliński <marek@goteleport.com>

--- client/client.go
+++ client/client.go
@@ -66,7 +66,7 @@ func (c *Client) GetJobStatus(ctx context.Context, jobID string) (pb.JobStatus, 
 		return pb.JobStatus_JOB_STATUS_UNSPECIFIED, nil, fmt.Errorf("failed to get job status: %w", err)
 	}
 
-	return resp.GetStatus(), resp.ExitCode, nil
+	return resp.GetStatus(), resp.GetExitCode(), nil
 }
 
 // StopJob stops a running job.
